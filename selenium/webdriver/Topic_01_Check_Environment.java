ee36ab97e69d57380cc99e5fec78408af7432c0f
admin <admin@192.168.31.106>
2021-11-14 06:25:42 +0700

    Update Topic 01 and add new Topic 03

--- selenium/webdriver/Topic_01_Check_Environment.java
+++ selenium/webdriver/Topic_01_Check_Environment.java
@@ -25,22 +25,22 @@ public class Topic_01_Check_Environment {
 	}
 
 	@Test
-	public void TC_01_ValidateCurrentUrl() {
+	public void TC_01() {
 		// Login Page Url matching
 		String loginPageUrl = driver.getCurrentUrl();
 		Assert.assertEquals(loginPageUrl, "https://www.facebook.com/");
 	}
 
 	@Test
-	public void TC_02_ValidatePageTitle() {
+	public void TC_02() {
 		// Login Page title
 		String loginPageTitle = driver.getTitle();
-		Assert.assertEquals(loginPageTitle, "Facebook � log in or sign up");
+		Assert.assertEquals(loginPageTitle, "Facebook – log in or sign up");
 	}
 
 	@Test
-	public void TC_03_LoginFormDisplayed() {
-		// Login form displayed
+	public void TC_03() {
+		// Login form displayed thêm chữ để đẩy code nào 
 		Assert.assertTrue(driver.findElement(By.xpath("//form[@data-testid='royal_login_form']")).isDisplayed());
 	}
 
