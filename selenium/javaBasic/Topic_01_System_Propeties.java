1aa9f7aae0b9018b81a940fd099a983ae7d5e966
admin <admin@192.168.31.106>
2021-11-13 14:57:38 +0700

    Init Repository

---
+++ selenium/javaBasic/Topic_01_System_Propeties.java
@@ -0,0 +1,12 @@
+package javaBasic;
+
+public class Topic_01_System_Propeties {
+	
+
+	public static void main(String[] args) {
+		String projectPath = System.getProperty("user.dir");
+		System.out.println(projectPath);
+
+	}
+
+}
