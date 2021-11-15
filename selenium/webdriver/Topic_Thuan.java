b43a0b840c0643d3a65eeb48d6760d1884f45556
Thuan Tran <thuantran8x@gmail.com>
2021-11-15 20:20:32 +0700

    Update Topic acb

--- selenium/webdriver/Topic_Thuan.java
+++ selenium/webdriver/Topic_Thuan.java
@@ -1,5 +1,5 @@
 package webdriver;
 
 public class Topic_Thuan {
-    System.out.println("minh chao cac ban");
+    System.out.println("minh chao cac ban, tuan roi ban qua nay moi thuc hanh cac ban a");
 }
